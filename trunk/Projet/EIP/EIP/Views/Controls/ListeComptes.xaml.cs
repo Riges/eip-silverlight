b25b432664247cd1a53e3ed7de5d27c732d926db
pocketino <pocketino@58143d36-feaa-11de-ac2f-e974ed891282>
2010-02-09 17:58:01 +0000

    Modif system login + ajout de comptes

---
+++ trunk/Projet/EIP/EIP/Views/Controls/ListeComptes.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EIP.Views.Controls
+{
+    public partial class ListeComptes : UserControl
+    {
+        private App app;
+        public ListeComptes()
+        {
+            InitializeComponent();
+            app = (App)System.Windows.Application.Current;
+            LoadAccountButtons();
+        }
+
+        private void LoadAccountButtons()
+        {
+            if (app.currentAccounts != null)
+            {
+                if (app.currentAccounts.Count > 0)
+                {
+                    foreach (Account oneAccount in app.currentAccounts)
+                    {
+                        Button btnAccount = new Button();
+                        btnAccount.Content = oneAccount.name;
+                        //btnAccount.Click += btnAccount_Click;
+                        btnAccount.CommandParameter = oneAccount;
+                        LayoutPanel.Children.Add(btnAccount);
+                    }
+                }
+            }
+        }
+
+       void btnAccount_Click(object sender, RoutedEventArgs e)
+        {
+            app.LoadAccount((Account)((Button)sender).CommandParameter);
+        }
+    }
+}
