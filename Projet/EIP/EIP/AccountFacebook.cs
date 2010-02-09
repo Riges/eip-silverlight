4b06d4e113dce6f0d2a9039986da06c2d44964da
pocketino <cbonfils@users.noreply.github.com>
2010-02-09 17:58:28 +0000

    Modif system login + ajout de comptes

---
+++ Projet/EIP/EIP/AccountFacebook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EIP
+{
+    public class AccountFacebook: Account
+    {
+        //public long accountID { get; set; }
+        //public TypeAccount typeAccount { get; set; }
+        //public long userID { get; set; }
+
+        public bool sessionExpires { get; set; }
+        public string sessionKey { get; set; }
+        public string sessionSecret { get; set; }
+
+
+
+        public AccountFacebook()
+        {
+            
+        }
+
+    
+    }
+}
