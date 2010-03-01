fafcba82d445de2bb0e996e42de0a4307a38c988
pocketino <cbonfils@users.noreply.github.com>
2010-03-01 14:53:32 +0000

    Ajout du projet EIPLibrary

---
+++ Projet/EIP/EIPLibrary/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace EIPLibrary
+{
+    public class Account
+    {
+        public long accountID { get; set; }
+        public TypeAccount typeAccount { get; set; }
+        public long userID { get; set; }
+        public string name { get; set; }
+
+
+        public enum TypeAccount
+        {
+            Facebook,
+            Twitter,
+            Myspace
+        }
+
+
+        public Account()
+        {
+
+        }
+
+
+    }
+}
---
+++ Projet/EIP/EIPLibrary/AccountTwitter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dimebrain.TweetSharp.Model;
+
+namespace EIPLibrary
+{
+    public class AccountTwitter : Account
+    {
+        //public long accountID { get; set; }
+        //public TypeAccount typeAccount { get; set; }
+        //public long userID { get; set; }
+
+        public string token { get; set; }
+        public string tokenSecret { get; set; }
+        public string pin { get; set; }
+        public TwitterUser user { get; set; }
+        public IEnumerable<TwitterStatus> homeStatuses { get; set; }
+
+        public AccountTwitter()
+        {
+
+        }
+
+        //*******************************\\
+        //*Methode de récupération d'infos*\\
+        //***********************************\\
+
+        /// <summary>
+        /// Met à jour l'attribut "homeStatuses"
+        /// </summary>
+        public void LoadHomeStatuses()
+        {
+            //Connexion.serviceEIP.TwitterGetHomeStatusesCompleted += new EventHandler<TwitterGetHomeStatusesCompletedEventArgs>(serviceEIP_TwitterGetHomeStatusesCompleted);
+            //Connexion.serviceEIP.TwitterGetHomeStatusesAsync(Connexion.consumerKey, Connexion.consumerSecret, this.token, this.tokenSecret);
+        }
+
+       /* private void serviceEIP_TwitterGetHomeStatusesCompleted(object sender, TwitterGetHomeStatusesCompletedEventArgs e)
+        {
+            if (this.typeAccount == Account.TypeAccount.Twitter && e != null && e.Result != null)
+            {
+                this.homeStatuses = e.Result;
+                Connexion.SaveAccount();
+            }
+        }*/
+
+
+    }
+}
