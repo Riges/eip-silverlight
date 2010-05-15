6704572987eb94c4e1af7ce6e2caf827559c01a8
aurelien.beduer <aurelien.beduer@58143d36-feaa-11de-ac2f-e974ed891282>
2010-05-15 15:59:59 +0000

    debut de l'objet friend et friendlist

---
+++ trunk/Projet/EIP/EIP/Objects/Friend.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Facebook.Rest;
+using Facebook.Schema;
+using TweetSharp.Model;
+
+namespace EIP.Objects
+{
+    public class Friend
+    {
+        public user userFB { get; set; }
+        public TwitterUser userTW { get; set; }
+
+
+        /// <summary>
+        /// methode aui per;et de renvoyer tq ;ere
+        /// </summary>
+        /// <param name="name">nom de ton papa</param>
+        public void toto(string name)
+        {
+
+        }
+
+
+    }
+}
