e7689ede42cd2b6db6ee5b37b5c6d8f23914dacc
aurelien.beduer <aurelien.beduer@58143d36-feaa-11de-ac2f-e974ed891282>
2010-07-16 14:46:12 +0000

    ProfilPage

---
+++ Projet/EIP/EIP/Views/ProfilPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using EIP.Objects;
+using Facebook.Schema;
+
+namespace EIP.Views
+{
+    public partial class ProfilPage : Page
+    {
+        public Dictionary<String, Profil> profil;
+
+        public ProfilPage()
+        {
+            InitializeComponent();
+            profil = new Dictionary<String, Profil>();
+            LoadProfil();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// methode qui merge les profils de l'user ou du friend
+        /// </summary>
+        private void LoadProfil()
+        {
+            foreach (KeyValuePair<long, AccountLight> account in Connexion.accounts)
+            {
+                switch (account.Value.account.typeAccount)
+                {
+                    case EIP.ServiceEIP.Account.TypeAccount.Facebook:
+                        //Profil toto = new Profil();
+                        //toto.profilFB = ;
+                        //profil.Add(toto, toto.profilFB.id);
+                        break;
+                    case EIP.ServiceEIP.Account.TypeAccount.Twitter:
+                        //Profil toto = new Profil();
+                        //toto.profilTW = ;
+                        //profil.Add(toto, );
+                        break;
+                    case EIP.ServiceEIP.Account.TypeAccount.Myspace:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+}
