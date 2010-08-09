7c8f28b42b42cee8079202949e363288a9fe55f5
pocketino <cbonfils@users.noreply.github.com>
2010-08-09 16:34:45 +0000

    update issue 9 Début remix albums dans profil

---
+++ Projet/EIP/EIP/Views/Controls/AlbumsControl.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Facebook.Schema;
+
+namespace EIP.Views.Controls
+{
+    public partial class AlbumsControl : UserControl
+    {
+        public AlbumsControl()
+        {
+            InitializeComponent();
+        }
+
+        public AlbumsControl(long accountID, long uid)
+        {
+            InitializeComponent();
+            LoadAlbums(accountID, uid);
+
+        }
+
+        public void LoadAlbums(long accountID, long uid)
+        {
+            if (Connexion.accounts != null && Connexion.accounts.Count > 0)
+            {
+                foreach (KeyValuePair<long, AccountLight> acc in Connexion.accounts)
+                {
+                    if (acc.Key == accountID)
+                        acc.Value.selected = true;
+                    else
+                        acc.Value.selected = false;
+                }
+                Connexion.listeComptes.ListeCompteMode = ListeComptes.ListeCptMode.ReadOnly;
+                Connexion.listeComptes.Reload();
+                
+
+
+                AccountFacebookLight account = (AccountFacebookLight)Connexion.accounts[accountID];
+                if (account.selected)
+                account.GetAlbumsCalled += new AccountFacebookLight.OnGetAlbumsCompleted(AlbumsView_GetAlbumsCalled);
+                account.GetAlbums(uid);
+            }
+
+        }
+
+          private void AlbumsView_GetAlbumsCalled(List<album> albums)
+        {
+            Connexion.dispatcher.BeginInvoke(() =>
+                {
+                    flowControl.DataContext = albums;
+                });
+        }
+
+
+    }
+}
